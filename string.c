17e8470ceee6c92ca45333b0c93950ef6bf4eaf4
Mike Krinkin <krinkin.m.u@gmail.com>
2015-11-11 20:32:17 +0300

    add memmove

--- string.c
+++ string.c
@@ -10,6 +10,21 @@ void *memcpy(void *dst, const void *src, size_t size)
 	return dst;
 }
 
+static void *memcpy_r(void *dst, const void *src, size_t size)
+{
+	char *d = dst;
+	const char *s = src;
+
+	d += size;
+	s += size;
+	while (size--)
+		*(--d) = *(--s);
+	return dst;
+}
+
+void *memmove(void *dst, const void *src, size_t size)
+{ return dst > src ? memcpy_r(dst, src, size) : memcpy(dst, src, size); }
+
 size_t strlen(const char *str)
 {
 	const char *pos = str;
--- string.h
+++ string.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 void *memcpy(void *dst, const void *src, size_t size);
+void *memmove(void *dst, const void *src, size_t size);
 size_t strlen(const char *str);
 char *strchr(const char *str, int c);
 
