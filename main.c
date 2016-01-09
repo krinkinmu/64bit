0be76431e235a7bad3f4150135ad7578ea681843
Mike Krinkin <krinkin.m.u@gmail.com>
2016-01-09 17:30:22 +0300

    deadlock test

--- main.c
+++ main.c
@@ -9,6 +9,15 @@
 #include "vga.h"
 #include "vfs.h"
 
+#include "stdio.h"
+
+static void test_deadlock(void)
+{
+	DEFINE_MUTEX(deadlock);	
+	mutex_lock(&deadlock);
+	mutex_lock(&deadlock);
+	DBG_ERR("Deadlock test failed");
+}
 
 void main(void)
 {
@@ -20,9 +29,13 @@ void main(void)
 	setup_alloc();
 	setup_time();
 	setup_threading();
+
+	local_irq_enable();
+
 	setup_vfs();
 	setup_ramfs();
 
-	local_irq_enable();
+	test_deadlock();
+
 	idle();
 }
