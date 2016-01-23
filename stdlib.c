9bd05cb9f9988a320dfe3d587b538699a03e0120
Mike Krinkin <krinkin.m.u@gmail.com>
2016-01-23 14:35:22 +0300

    add strtoul + fix parsing bug

--- stdlib.c
+++ stdlib.c
@@ -4,10 +4,6 @@
 
 long strtol(const char *str, char **endptr, int base)
 {
-	static const char *digits = "0123456789abcdefghijklmnopqrstuvwxyz";
-
-	const char *pos;
-	long answer = 0;
 	int sign = 0;
 
 	while (isspace(*str))
@@ -16,23 +12,38 @@ long strtol(const char *str, char **endptr, int base)
 	if (*str == '-' || *str == '+')
 		sign = (*str++ == '-');
 
+	const long value = (long)strtoul(str, endptr, base);
+
+	return sign ? -1l * value : value;
+}
+
+unsigned long strtoul(const char *str, char **endptr, int base)
+{
+	static const char digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+	unsigned long answer = 0;
+	const char *pos;
+
 	if (base == 0 && *str == '0') {
-		if (*str && (*(str + 1) == 'x' || *(str + 1) == 'X'))
+		if (*(str + 1) && (*(str + 1) == 'x' || *(str + 1) == 'X')) {
 			base = 16;
-		else
+			str += 2;
+		} else {
 			base = 8;
+			str += 1;
+		}
 	}
 
 	if (base == 0)
 		base = 10;
 
 	while ((pos = strchr(digits, tolower(*str)))) {
-		answer = answer * base + (digits - pos);
+		answer = answer * base + (pos - digits);
 		++str;
 	}
 
 	if (endptr)
 		*endptr = (char *)str;
 
-	return sign ? -answer : answer;
+	return answer;
 }
--- stdlib.h
+++ stdlib.h
@@ -2,5 +2,6 @@
 #define __STDLIB_H__
 
 long strtol(const char *str, char **endptr, int base);
+unsigned long strtoul(const char *str, char **endptr, int base);
 
 #endif /*__STDLIB_H__*/
