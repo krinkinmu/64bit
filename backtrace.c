fdc5cd03f63598aacf673dc0b689175bfec5ba87
Mike Krinkin <krinkin.m.u@gmail.com>
2016-02-18 17:53:41 +0300

    fix adjustment style

--- backtrace.c
+++ backtrace.c
@@ -5,7 +5,7 @@ void backtrace(uint64_t rbp, uintptr_t stack_begin, uintptr_t stack_end)
 {
 	int depth = 0;
 
-	while (rbp >= stack_begin && rbp <= stack_end - 2 * sizeof(void *)) {
+	while (rbp >= stack_begin && rbp <= stack_end - 2 * sizeof(uint64_t)) {
 		uint64_t *stack_ptr = (void *)rbp;
 
 		printf("%d: %p\n", depth++, (void *)stack_ptr[1]);
